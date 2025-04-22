b04c4be1101f609d2910d3cffa05568c68fb51fa
hmachefe <hugo.machefer@gmail.com>
2025-04-22 19:16:15 -0400

    create schema populated with typDefs

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
+// types
+import { typeDefs } from "./schema.js";
+
 // server setup 
 const server = new ApolloServer({
-    // typeDefs
+    typeDefs
     // resolvers
 });
 
 const { url } = await startStandaloneServer(server, {
     listen: {
-        post: 4000
+        port: 4000
     }
 });
 
