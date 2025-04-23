35e7d111d8c2c12229b7fdbca4d004286661cd0b
hmachefe <hugo.machefer@gmail.com>
2025-04-23 16:53:47 -0400

    add variable to query (id)  to target one single item like game or author etc

--- index.js
+++ index.js
@@ -12,11 +12,20 @@ const resolvers = {
         games() {
             return db.games;
         },
+        game(parent, args, context) {
+            return db.games.find(game => game.id === args.id);
+        },
         reviews() {
             return db.reviews;
         },
+        review(parent, args, context) {
+            return db.reviews.find(review => review.id === args.id);
+        },
         authors() {
             return db.authors;
+        },
+        author(parent, args, context) {
+            return db.authors.find(author => author.id === args.id);
         }
     }
 }
