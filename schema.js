2499e0b49811af6c2efccdb501bbf346b4f7b80f
hmachefe <hugo.machefer@gmail.com>
2025-04-24 10:29:40 -0400

    update game mutations

--- schema.js
+++ schema.js
@@ -29,11 +29,16 @@ export const typeDefs = `#graphql
     type Mutation {
         deleteGame(id: ID!): [Game]
         addGame(game: AddGameInput!): [Game]
+        updateGame(id: ID!, edits: EditGameInput!): [Game]
     }
     input AddGameInput {
         title: String!
-        platform: [String!]
+        platform: [String!]!
     }    
+    input EditGameInput {
+        title: String
+        platform: [String!]
+    }
 `
 
 
